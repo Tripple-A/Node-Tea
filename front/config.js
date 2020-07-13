774e43f0deb305aa6a82e7ec7aa48c0a3236fad2
Tripple-A <biodun9@gmail.com>
2020-07-13 14:45:28 +0100

    added http server listen function

--- front/config.js
+++ front/config.js
@@ -1,5 +1,11 @@
 const env = process.env;
 
+export const logStars = function(message) {
+  console.info('**********');
+  console.info(message);
+  console.info('**********');
+};
+
 export default {
   port: env.PORT || 8080,
   nodeEnv: env.NODE_ENV || 'development'
--- front/server.js
+++ front/server.js
@@ -1,4 +1,25 @@
-import config from './config';
+import https from 'https';
+import http from 'http';
+import config, { logStars } from './config';
 
-console.log(config);
+https.get('https://www.lynda.com',res => {
+  //console.log('response status:', res.statusCode);
+
+  res.on('data',chunk => {
+    // console.log(chunk.toString());
+  });
+});
+
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+      res.write('I can stream!\n');
+      res.end();
+    }, 3000);
+  });
+
+server.listen(config.port);
+
+//logStars('Biodun the great');
+
 
