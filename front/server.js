739500aff83488ef013696f81f7c2aa348729cc6
Tripple-A <biodun9@gmail.com>
2020-07-14 12:14:48 +0100

    added express routing for api

--- front/server.js
+++ front/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import apiRouter from './api';
 import config from './config';
 const server = express();
 
 server.get('/', (req,res) => {
-  res.send('Biodun is the greatest')
+  res.send('Biodun is the greatest');
 });
 
 
 server.use(express.static('public'));
+server.use('/api',apiRouter);
 
 server.listen(config.port,() => {
     console.info(`listening on porte ${config.port}`);
