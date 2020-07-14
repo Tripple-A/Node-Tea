82213c6c699595860c82265fac3ae0208896f835
Tripple-A <biodun9@gmail.com>
2020-07-14 12:07:56 +0100

    added static files to public folder for ease of serving

--- front/server.js
+++ front/server.js
@@ -1,20 +1,14 @@
 import express from 'express';
 import config from './config';
-import fs from 'fs';
 const server = express();
 
 server.get('/', (req,res) => {
   res.send('Biodun is the greatest')
 });
 
-server.get('/about', (req,res) => {
-    fs.readFile('./about.html',(err,data) => {
-        console.log(data)
-        res.send(data.toString());
-    })
-  });
 
-  server.use(express.static('public'));
+server.use(express.static('public'));
+
 server.listen(config.port,() => {
     console.info(`listening on porte ${config.port}`);
 })
